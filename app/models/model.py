b56062a5729f99346c6e7870c772e53b2b03d224
EC2 Default User <ec2-user@ip-172-31-43-82.us-east-2.compute.internal>
2019-07-18 17:07:36 +0000

    finished completely futbol

--- app/models/model.py
+++ app/models/model.py
@@ -1,19 +1,45 @@
+def soccer_quiz(position, goal, mindset, team, best):
+    messi_score=0
+    modric_score=0
+    van_score=0
+    if position == "Striker":
+        messi_score += 1
+    elif position == "Centermid":
+        modric_score += 1
+    elif position == "Centerback":
+        van_score += 1
+    elif goal == "Dribble past the goalie":
+        messi_score += 1
+    elif goal == "Pass the ball into the goal":
+        modric_score += 1
+    elif goal == "Header":
+        van_score += 1
+    elif mindset == "Be the difference":
+        messi_score += 1
+    elif mindset == "Help the game flow":
+        modric_score += 1
+    elif mindset == "Defense":
+        van_score += 1 
+    elif team == "Barcelona":
+        messi_score += 1
+    elif team == "Madrid":
+        modric_score += 1
+    elif team == "Liverpool":
+        van_score += 1
+    elif best == "Quick as lightining":
+        messi_score += 1
+    elif best == "Run for miles":
+        modric_score += 1
+    elif best == "Human tank":
+        van_score += 1
+    if messi_score > modric_score and messi_score > van_score:
+        return "You are Messi"
+    elif modric_score > messi_score and modric_score > van_score:
+        return "You are Modric"
+    elif van_score > messi_score and van_score > modric_score:
+        return "You are Van Dijk"
 
-messi_score=0
-messi_scroe=int(messi_score)
-modric_score=0
-modric_scroe=int(modric_score)
-van_score=0
-van_score=int(van_score)
-print("What is your favorite mosition? \n1. Striker \n2. Centermid \n3. Centerback")
-a1 = "1"
-a2 = "2"
-response1 = input("Your answer is:"):
-    if response1 = a1:
-        return messi_score = messi_score + 1
-    if response1 = a2:
-        return modric_score = modric_score + 1
-    else: 
-        return van_score = van_score + 1
+
+
 
 
--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,23 @@ from app.models import model, formopener
 @app.route('/index')
 def index():
     return render_template("home.html")
-@app.route("/futbol")
-def futbol():
-    return render_template("futbol.html")
 @app.route("/hamburger")
 def hamburger():
     return render_template("hamburger.html")
 @app.route("/music")
 def music():
     return render_template("music.html")
+@app.route("/futbol", methods=["GET", "POST"])
+def futbol():
+    if request.method == "GET":
+        return render_template("/futbol.html")
+    else:
+        userdata = formopener.dict_from(request.form)
+        position = userdata["position"].decode('utf-8')
+        goal = userdata["goal"].decode('utf-8')
+        mindset = userdata["mindset"].decode('utf-8')
+        team = userdata["team"].decode('utf-8')
+        best = userdata["best"].decode('utf-8')
+        print(userdata)
+        futbol = str(model.soccer_quiz(position, goal, mindset, team, best))
+        return render_template("/futbol.html", futbol = futbol)
