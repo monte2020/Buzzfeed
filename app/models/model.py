241910ee54184f8dc2694750b5c25a5c0fbac0b4
EC2 Default User <ec2-user@ip-172-31-43-82.us-east-2.compute.internal>
2019-07-18 15:05:45 +0000

    futbol quiz

---
+++ app/models/model.py
@@ -0,0 +1,19 @@
+
+messi_score=0
+messi_scroe=int(messi_score)
+modric_score=0
+modric_scroe=int(modric_score)
+van_score=0
+van_score=int(van_score)
+print("What is your favorite mosition? \n1. Striker \n2. Centermid \n3. Centerback")
+a1 = "1"
+a2 = "2"
+response1 = input("Your answer is:"):
+    if response1 = a1:
+        return messi_score = messi_score + 1
+    if response1 = a2:
+        return modric_score = modric_score + 1
+    else: 
+        return van_score = van_score + 1
+
+
--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,13 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template("home.html")
+@app.route("/futbol")
+def futbol():
+    return render_template("futbol.html")
+@app.route("/hamburger")
+def hamburger():
+    return render_template("hamburger.html")
+@app.route("/music")
+def music():
+    return render_template("music.html")
